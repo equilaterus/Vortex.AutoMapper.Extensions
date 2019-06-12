372d56ce78e81dcfdc3f016b87bfb0857f5001ac
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-06-11 20:41:09 -0500

    Added MaybeMap test

--- test/MapperExtensions.Test.cs
+++ test/MapperExtensions.Test.cs
@@ -23,5 +23,17 @@ namespace Vortex.AutoMapper.Extensions.Test
             Assert.IsType<Maybe<double>>(result);
             Assert.Equal(1.2, result.Match(x => x, 0));
         }
+
+        [Fact]
+        public void MaybeMap_Null_ThrowsException()
+        {
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<object>(It.IsAny<object>()))
+                .Returns(null);
+
+            var objectMock = mapperMock.Object;           
+
+            Assert.Throws<ArgumentNullException>(() => objectMock.MaybeMap<object>(new object()));
+        }
     }
 }
