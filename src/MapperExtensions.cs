1d4dfcd7573b83ef1fe0dda833cdb2cd50a40477
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-30 12:58:35 -0500

    Adding additional IMapper methods
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- src/MapperExtensions.cs
+++ src/MapperExtensions.cs
@@ -14,10 +14,34 @@ namespace Equilaterus.Vortex.AutoMapper.Extensions
             return new Maybe<TDestination>(mapper.Map<TDestination>(source));
         }
 
+        public static Maybe<TDestination> MaybeMap<TDestination>(
+            this IMapper mapper, object source, Action<IMappingOperationOptions> opts)
+        {
+            return new Maybe<TDestination>(mapper.Map<TDestination>(source, opts));
+        }
+
+        public static Maybe<TDestination> MaybeMap<TSource, TDestination>(
+            this IMapper mapper, TSource source)
+        {
+            return new Maybe<TDestination>(mapper.Map<TSource, TDestination>(source));
+        }
+
+        public static Maybe<TDestination> MaybeMap<TSource, TDestination>(
+            this IMapper mapper, TSource source, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+        {
+            return new Maybe<TDestination>(mapper.Map<TSource, TDestination>(source, opts));
+        }
+
         public static Maybe<TDestination> MaybeMap<TSource, TDestination>(
             this IMapper mapper, TSource source, TDestination destination)
         {
             return new Maybe<TDestination>(mapper.Map(source, destination));
+        }
+
+        public static Maybe<TDestination> MaybeMap<TSource, TDestination>(
+            this IMapper mapper, TSource source, TDestination destination, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+        {
+            return new Maybe<TDestination>(mapper.Map(source, destination, opts));
         }
     }
 }
