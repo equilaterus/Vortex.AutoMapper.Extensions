070a652778599c22eb87c56644ed518b694578c8
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-30 13:12:50 -0500

    Added MaybeMapOpt tests
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- test/MapperExtensions.Test.cs
+++ test/MapperExtensions.Test.cs
@@ -149,5 +149,33 @@ namespace Equilaterus.Vortex.AutoMapper.Extensions.Test
             // Execute
             Assert.Throws<ArgumentNullException>(() => mapperMock.Object.MaybeMap<object, object>(null, new object()));
         }
+
+        [Fact]
+        public void MaybeMapOpt_SourceDestination_Success()
+        {
+            // Prepare
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map("1", 0.2, null))
+                .Returns(1.2);
+
+            // Execute
+            var result = mapperMock.Object.MaybeMap("1", 0.2, null);
+
+            // Verify
+            Assert.IsType<Maybe<double>>(result);
+            Assert.Equal(1.2, result.Match(x => x, 0));
+        }
+
+        [Fact]
+        public void MaybeMapOpt_SourceDestination_Null_ThrowsException()
+        {
+            // Prepare
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<object, object>(null, null, null))
+                .Returns(null);
+
+            // Execute
+            Assert.Throws<ArgumentNullException>(() => mapperMock.Object.MaybeMap<object, object>(null, new object(), null));
+        }
     }
 }
